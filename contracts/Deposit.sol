e1feaa6847312e573bbf86889c1e7604bf2a73b0
xtrone88 <prime18michael@gmail.com>
2022-05-13 14:26:40 -0500

    updated for balance testing

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -3,18 +3,26 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Deposit {
 
-  address public operatorAddress;
+  // address public operatorAddress;
   mapping(address => uint256) public deposits;
 
-  constructor(address _operatorAddress) {
-    operatorAddress = _operatorAddress;
+  constructor(/*address _operatorAddress*/) {
+    // operatorAddress = _operatorAddress;
   }
 
   event SuccessDeposit(string trnasactionId, uint256 amount, address receiver, address sender);
 
   function deposit(address _receiver, string memory transactionId) payable public {
     deposits[_receiver] += msg.value;
-    require(payable(operatorAddress).send(msg.value));
+    // require(payable(operatorAddress).send(msg.value));
     emit SuccessDeposit(transactionId, msg.value, _receiver, msg.sender);
+  }
+
+  function getBalance() public view returns (uint256) {
+    return address(this).balance;
+  }
+
+  function getBalance2(address addr) public view returns (uint256) {
+    return address(addr).balance;
   }
 }
--- migrations/3_deploy_deposit.js
+++ migrations/3_deploy_deposit.js
@@ -1,6 +1,11 @@
 const DepositContract = artifacts.require('Deposit')
 
 module.exports = async function(deployer, network, accounts) {
-    // await deployer.deploy(DepositContract)
-    // const deposit = await DepositContract.deployed()
+    await deployer.deploy(DepositContract)
+    const deposit = await DepositContract.deployed()
+    await deposit.deposit(deposit.address, '0x0324202', {value: '1000'})
+    
+    console.log(await deposit.getBalance2(deposit.address))
+    console.log(await deposit.deposits(deposit.address))
+    console.log(await deposit.getBalance())
 }
